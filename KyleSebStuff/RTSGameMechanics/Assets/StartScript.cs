da2d018ee6e4b0989dc0ed4e4c689b4fbb56b733
jjhsiung91 <jjhsiung91@gmail.com>
2014-04-04 00:45:14 -0400

    attempt 2

--- KyleSebStuff/RTSGameMechanics/Assets/StartScript.cs
+++ KyleSebStuff/RTSGameMechanics/Assets/StartScript.cs
@@ -5,8 +5,8 @@ using SSGameEvents;
 
 public class StartScript : MonoBehaviour {
 
-	public Object opponentUnit;
-	public Object unitPrefab;
+	public Object greenUnit;
+	public Object blueUnit;
 
 	void Start () {
 		Dispatcher.Instance.Register (this);
@@ -24,16 +24,21 @@ public class StartScript : MonoBehaviour {
 		playerObject.GetComponent<PlayerScript>().id = connectionEvent.ID;
 		playerObject.GetComponent<UserInputManager>().playerID = connectionEvent.ID;
 
-		GameObject unit = (GameObject)Instantiate (unitPrefab, new Vector3(18, 2.5f, 27), Quaternion.identity);
-		unit.GetComponent<WorldObject>().playerID = connectionEvent.ID;
-
-
 		GameObject opponentObject = GameObject.Find("Opponent");
 		opponentObject.GetComponent<UserInputManager>().playerID = connectionEvent.opponentID;
 
-		
-		GameObject unit2 = (GameObject)Instantiate (opponentUnit, new Vector3(20, 2.5f, 30), Quaternion.identity);
-		unit2.GetComponent<WorldObject>().playerID = connectionEvent.opponentID;
+		GameObject myUnit;
+		GameObject opponentUnit;
+		if (connectionEvent.ID == 1) {
+			myUnit = (GameObject)Instantiate(blueUnit, new Vector3(18, 2.5f, 27), Quaternion.identity);
+			opponentUnit = (GameObject)Instantiate (greenUnit, new Vector3(20, 2.5f, 30), Quaternion.identity);
+		} else {
+			opponentUnit = (GameObject)Instantiate(blueUnit, new Vector3(18, 2.5f, 27), Quaternion.identity);
+			myUnit = (GameObject)Instantiate (greenUnit, new Vector3(20, 2.5f, 30), Quaternion.identity);
+		}
+
+		myUnit.GetComponent<WorldObject>().playerID = connectionEvent.ID;
+		opponentUnit.GetComponent<WorldObject>().playerID = connectionEvent.opponentID;
 
 		SSGameSetup.Ready(connectionEvent.ID);
 	}
