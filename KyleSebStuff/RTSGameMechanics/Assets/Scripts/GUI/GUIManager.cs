cc093486658722b13cfca097ea25b791cd92cedd
Sebastian Martinez <SebastianMartinez@Sebastian-Martinezs-MacBook-Pro.local>
2014-04-04 15:02:40 -0400

    Moved DragSelectBox visual to GUIManager

--- KyleSebStuff/RTSGameMechanics/Assets/Scripts/GUI/GUIManager.cs
+++ KyleSebStuff/RTSGameMechanics/Assets/Scripts/GUI/GUIManager.cs
@@ -16,15 +16,34 @@ public class GUIManager : MonoBehaviour {
     public Texture2D POWER_ICON;
     public Texture2D MEMORY_ICON;
 
+	//Dragging GUI variables
+	private GUIStyle m_DragStyle = new GUIStyle();
+	private Vector2 m_DragLocationStart;
+	private Vector2 m_DragLocationEnd;
+	private bool m_CheckDeselect = false;
+	private bool m_Dragging = false;
+
     //Initialization into GUIResources
     void Start() {
         player = this.gameObject.GetComponent<PlayerScript>();
         GUIResources.SELECT_BOX_SKIN = SELECT_BOX_SKIN;
+
+		//Initialize the Dragging Variables
+		m_DragStyle.normal.background = TextureGenerator.MakeTexture (0.8f, 0.8f, 0.8f, 0.3f);
+		m_DragStyle.border.bottom = 1;
+		m_DragStyle.border.top = 1;
+		m_DragStyle.border.left = 1;
+		m_DragStyle.border.right = 1;
     }
     
     void OnGUI() {
         DrawOrdersBar();
         DrawResourceBar();
+
+		if (m_Dragging) {
+			m_DragLocationEnd = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+			DragBox (m_DragLocationStart, m_DragLocationEnd, m_DragStyle);
+		}
     }
     
     /*** Private Worker Methods ***/
@@ -57,4 +76,54 @@ public class GUIManager : MonoBehaviour {
         GUI.DrawTexture(new Rect(iconLeft, topPos, GUIResources.ICON_WIDTH, GUIResources.ICON_HEIGHT), resourceIcon);
         GUI.Label(new Rect(textLeft, topPos, GUIResources.TEXT_WIDTH, GUIResources.TEXT_HEIGHT), text);
     }
+
+	void Update() {
+		checkIfDragging ();
+	}
+
+	private void checkIfDragging() {
+		if (Input.GetMouseButtonDown (0))
+		{
+			m_DragLocationStart = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+			m_CheckDeselect = true;
+		}
+		
+		if (Input.GetMouseButtonUp (0))
+		{
+			m_CheckDeselect = false;
+			m_Dragging = false;
+		}
+		
+		if (m_CheckDeselect)
+		{
+			if (Mathf.Abs (Input.mousePosition.x - m_DragLocationStart.x) > 2 && Mathf.Abs (Input.mousePosition.y-m_DragLocationStart.y) > 2)
+			{
+				m_CheckDeselect = false;
+				m_Dragging = true;
+			}
+		}
+	}
+
+	public void DragBox(Vector2 topLeft, Vector2 bottomRight, GUIStyle style) {
+		float minX = Mathf.Max (topLeft.x, bottomRight.x);
+		float maxX = Mathf.Min (topLeft.x, bottomRight.x);
+		
+		float minY = Mathf.Max (Screen.height-topLeft.y, Screen.height-bottomRight.y);
+		float maxY = Mathf.Min (Screen.height-topLeft.y, Screen.height-bottomRight.y);
+		
+		Rect rect = new Rect(minX, minY, maxX-minX, maxY-minY);
+		
+		//Don't let the dragged area interfere with the gui
+		//		if (rect.xMin > Screen.width-m_GuiWidth)
+		//		{
+		//			rect.xMin = Screen.width-m_GuiWidth;
+		//		}
+		//		
+		//		if (rect.xMax > Screen.width-m_GuiWidth)
+		//		{
+		//			rect.xMax = Screen.width-m_GuiWidth;
+		//		}
+
+		GUI.Box (rect, "", style);
+	}
 }
