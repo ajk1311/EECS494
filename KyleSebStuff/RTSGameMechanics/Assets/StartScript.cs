58cfaa4b9c05cf697207bfe3a9814a5aef28c10a
Anthony Kause <akause@barracudanetworks.com>
2014-04-04 16:16:58 -0400

    Added more units to start test

--- KyleSebStuff/RTSGameMechanics/Assets/StartScript.cs
+++ KyleSebStuff/RTSGameMechanics/Assets/StartScript.cs
@@ -8,6 +8,9 @@ public class StartScript : MonoBehaviour {
 	public Object greenUnit;
 	public Object blueUnit;
 
+	private Vector3 startPosition = new Vector3(18, 2.5f, 27);
+	private Vector3 opponentStartPosition = new Vector3(20, 2.5f, 30);
+
 	void Start () {
 		Dispatcher.Instance.Register (this);
 		SSGameSetup.ConnectToGame("jjhsiung");
@@ -27,18 +30,24 @@ public class StartScript : MonoBehaviour {
 		GameObject opponentObject = GameObject.Find("Opponent");
 		opponentObject.GetComponent<UserInputManager>().playerID = connectionEvent.opponentID;
 
-		GameObject myUnit;
-		GameObject opponentUnit;
+		GameObject myUnit, myUnit2;
+		GameObject opponentUnit, opponentUnit2;
 		if (connectionEvent.ID == 1) {
-			myUnit = (GameObject)Instantiate(blueUnit, new Vector3(18, 2.5f, 27), Quaternion.identity);
-			opponentUnit = (GameObject)Instantiate (greenUnit, new Vector3(20, 2.5f, 30), Quaternion.identity);
+			myUnit = (GameObject)Instantiate(blueUnit, startPosition, Quaternion.identity);
+			myUnit2 = (GameObject)Instantiate(blueUnit, startPosition + new Vector3(-1.5f, 0, 0), Quaternion.identity);
+			opponentUnit = (GameObject)Instantiate(greenUnit, opponentStartPosition, Quaternion.identity);
+			opponentUnit2 = (GameObject)Instantiate(greenUnit, opponentStartPosition + new Vector3(1.5f, 0, 0), Quaternion.identity);
 		} else {
-			opponentUnit = (GameObject)Instantiate(blueUnit, new Vector3(18, 2.5f, 27), Quaternion.identity);
-			myUnit = (GameObject)Instantiate (greenUnit, new Vector3(20, 2.5f, 30), Quaternion.identity);
+			myUnit = (GameObject)Instantiate(blueUnit, opponentStartPosition, Quaternion.identity);
+			myUnit2 = (GameObject)Instantiate(blueUnit, opponentStartPosition + new Vector3(1.5f, 0, 0), Quaternion.identity);
+			opponentUnit = (GameObject)Instantiate(blueUnit, startPosition, Quaternion.identity);
+			opponentUnit2 = (GameObject)Instantiate(blueUnit, startPosition + new Vector3(-1.5f, 0, 0), Quaternion.identity);
 		}
 
 		myUnit.GetComponent<WorldObject>().playerID = connectionEvent.ID;
+		myUnit2.GetComponent<WorldObject>().playerID = connectionEvent.ID;
 		opponentUnit.GetComponent<WorldObject>().playerID = connectionEvent.opponentID;
+		opponentUnit2.GetComponent<WorldObject>().playerID = connectionEvent.opponentID;
 
 		SSGameSetup.Ready(connectionEvent.ID);
 	}
