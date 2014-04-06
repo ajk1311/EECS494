b7cd7465ad445f3acf825ef56bb62f8213a3da98
jjhsiung91 <jjhsiung91@gmail.com>
2014-04-06 18:38:04 -0400

    modified Building/Destructable

--- KyleSebStuff/RTSGameMechanics/Assets/Scripts/Objects/Buildings/DestructableBuilding.cs
+++ KyleSebStuff/RTSGameMechanics/Assets/Scripts/Objects/Buildings/DestructableBuilding.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class DestructableBuilding : Building {
 
-	protected virtual void OnDestroy() {
+	public override void GameUpdate (float deltaTime)
+	{
+		base.GameUpdate (deltaTime);
+		if(hitPoints <= 0) {
+			Destroy();
+		}
+	}
+	protected virtual void Destroy() {
 		//change graphic
 		//make sure the building unit can no longer be clicked
 	}
