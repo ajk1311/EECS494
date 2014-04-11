568a99e6ba76197d718dd2ebf18362ec29fea0e9
Anthony Kause <akause@barracudanetworks.com>
2014-04-11 00:29:44 -0400

    More int conversions in unit, ranged, and projectile

--- KyleSebStuff/RTSGameMechanics/Assets/Scripts/Objects/Projectiles/Projectile.cs
+++ KyleSebStuff/RTSGameMechanics/Assets/Scripts/Objects/Projectiles/Projectile.cs
@@ -8,34 +8,31 @@ public class Projectile : MonoBehaviour, SSGameManager.IUpdatable {
 	protected GameObject target;
 	public float speed = 0;
 	public int damageInflicted;
-	// Use this for initialization
+
 	protected virtual void Start () {
 		SSGameManager.Register(this);
         target = this.transform.parent.GetComponent<Unit>().currentTarget;
 		damageInflicted = this.transform.parent.GetComponent<Ranged>().damageInflicted;
 	}
 
-	// Update is called once per frame
 	public virtual void GameUpdate(float deltaTime) {
 		if(target == null) {
-			Destroy (gameObject);
-		}
-		else {
+			Destroy(gameObject);
+		} else {
 			Vector3 targetPosition = RTSGameMechanics.FindTransform(target.transform, "Target").position;
-			Int3 direction = (Int3) (targetPosition - transform.position).normalized;
-			direction *= speed * deltaTime;
-			transform.Translate((Vector3) direction);
+			float distance = ((Int3) transform.position - (Int3) targetPosition).magnitude / Int3.FloatPrecision;
+			if (distance < 0.1f) {
+				target.GetComponent<WorldObject>().TakeDamage(damageInflicted);
+				Destroy(gameObject);
+			} else {
+				Int3 direction = (Int3) (targetPosition - transform.position).normalized;
+				direction *= speed * deltaTime;
+				transform.Translate((Vector3) direction);
+			}
 		}
 	}
 
 	protected virtual void OnDestroy() {
 		SSGameManager.Unregister(this);
-	}
-
-	void OnTriggerEnter(Collider other){
-		if(other.gameObject == target){
-            target.GetComponent<WorldObject>().TakeDamage(damageInflicted);
-			Destroy(gameObject);
-		}
 	}
 }
