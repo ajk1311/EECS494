b4d4403a22691e5971943629ddf4b929ca55d891
TotallyKyle <kylez@umich.edu>
2014-04-15 18:50:01 -0400

    New Cursors and Different Color Drag
    Needs testing for cursors though.

--- KyleSebStuff/RTSGameMechanics/Assets/Scripts/GUI/GUIManager.cs
+++ KyleSebStuff/RTSGameMechanics/Assets/Scripts/GUI/GUIManager.cs
@@ -35,15 +35,15 @@ public class GUIManager : MonoBehaviour {
 
 
     //Dragging GUI variables
-    private static GUIModelManager.GUIModel mCurrentGuiModel;
-    private GUIStyle mDragStyle = new GUIStyle();
-    private Vector2 mDragLocationStart;
-    private Vector2 mDragLocationEnd;
-    private bool mCheckDeselect = false;
-    private static bool sDragging = false;
+    public GUISkin dragSelectSkin;
+    private static GUIModelManager.GUIModel currentGUIModel;
+    private Vector2 dragLocationStart;
+    private Vector2 dragLocationEnd;
+    private bool checkSelect = false;
+    private static bool isDragging = false;
 
     public static bool Dragging {
-        get { return sDragging; }
+        get { return isDragging; }
     }
 
     //Initialization into GUIResources
@@ -52,11 +52,11 @@ public class GUIManager : MonoBehaviour {
         GUIResources.SelectBoxSkin = selectBoxSkin;
 
         //Initialize the Dragging Variables
-        mDragStyle.normal.background = TextureGenerator.MakeTexture(0.8f, 0.8f, 0.8f, 0.3f);
-        mDragStyle.border.bottom = 1;
-        mDragStyle.border.top = 1;
-        mDragStyle.border.left = 1;
-        mDragStyle.border.right = 1;
+//        dragStyle.normal.background = TextureGenerator.MakeTexture(0.8f, 0.8f, 0.8f, 0.3f);
+//        dragStyle.border.bottom = 1;
+//        dragStyle.border.top = 1;
+//        dragStyle.border.left = 1;
+//        dragStyle.border.right = 1;
 
         //Initialize CursorState
         SetCursorState(CursorState.Select);
@@ -74,12 +74,12 @@ public class GUIManager : MonoBehaviour {
         DrawCurrentGUIModel();
         DrawResourceBar();
         DrawMouseCursor();
-        if (sDragging) {
+        if (isDragging) {
             float padding = GUIResources.GetScaledPixelSize(4);
-            mDragLocationEnd = new Vector2(
+            dragLocationEnd = new Vector2(
                 Mathf.Min(Mathf.Max(Input.mousePosition.x, padding), Screen.width - padding), 
                 Mathf.Min(Mathf.Max(Input.mousePosition.y, GUIResources.OrdersBarHeight + padding), Screen.height - padding));
-            DragBox(mDragLocationStart, mDragLocationEnd, mDragStyle);
+            DragBox(dragLocationStart, dragLocationEnd, dragSelectSkin);
         }
     }
 
@@ -193,12 +193,12 @@ public class GUIManager : MonoBehaviour {
     }
 
     private void DrawCurrentGUIModel() {
-        mCurrentGuiModel = GUIModelManager.GetCurrentModel(player.id);
-        if (mCurrentGuiModel == null) {
+        currentGUIModel = GUIModelManager.GetCurrentModel(player.id);
+        if (currentGUIModel == null) {
             return;
         }
 
-        if (!mCurrentGuiModel.cached) {
+        if (!currentGUIModel.cached) {
             float panelWidth = Screen.width / 3;
 
             float innerPadding = GUIResources.GetScaledPixelSize(2);
@@ -208,38 +208,38 @@ public class GUIManager : MonoBehaviour {
             float buttonHeight = GUIResources.GetScaledPixelSize(48);
             float buttonMinWidth = GUIResources.GetScaledPixelSize(100);
             
-            float leftButtonWidth = (panelWidth - 1.5f * horizontalPadding - innerPadding * (mCurrentGuiModel.leftPanelColumns - 1)) / mCurrentGuiModel.leftPanelColumns;
-            float centerButtonWidth = (panelWidth - horizontalPadding - innerPadding * (mCurrentGuiModel.leftPanelColumns - 1)) / mCurrentGuiModel.centerPanelColumns;
+            float leftButtonWidth = (panelWidth - 1.5f * horizontalPadding - innerPadding * (currentGUIModel.leftPanelColumns - 1)) / currentGUIModel.leftPanelColumns;
+            float centerButtonWidth = (panelWidth - horizontalPadding - innerPadding * (currentGUIModel.leftPanelColumns - 1)) / currentGUIModel.centerPanelColumns;
 
             float initialX = horizontalPadding;
             float initialY = Screen.height - GUIResources.OrdersBarHeight + verticalPadding;
 
-            for (int i = 0, len = mCurrentGuiModel.leftPanelButtons.Count; i < len; i++) {
-                int row = i / mCurrentGuiModel.leftPanelColumns;
-                int column = i % mCurrentGuiModel.leftPanelColumns;
+            for (int i = 0, len = currentGUIModel.leftPanelButtons.Count; i < len; i++) {
+                int row = i / currentGUIModel.leftPanelColumns;
+                int column = i % currentGUIModel.leftPanelColumns;
                 float buttonX = initialX + column * leftButtonWidth + (column > 0 ? innerPadding : 0);
                 float buttonY = initialY + row * buttonHeight + (row > 0 ? innerPadding : 0);
-                mCurrentGuiModel.leftPanelButtons [i].rect = new Rect(buttonX, buttonY, leftButtonWidth, buttonHeight);
+                currentGUIModel.leftPanelButtons [i].rect = new Rect(buttonX, buttonY, leftButtonWidth, buttonHeight);
             }
 
             initialX = panelWidth + horizontalPadding / 2;
-            for (int i = 0, len = mCurrentGuiModel.centerPanelButtons.Count; i < len; i++) {
-                int row = i / mCurrentGuiModel.centerPanelColumns;
-                int column = i % mCurrentGuiModel.centerPanelColumns;
+            for (int i = 0, len = currentGUIModel.centerPanelButtons.Count; i < len; i++) {
+                int row = i / currentGUIModel.centerPanelColumns;
+                int column = i % currentGUIModel.centerPanelColumns;
                 float buttonX = initialX + column * centerButtonWidth + (column > 0 ? innerPadding : 0);
                 float buttonY = initialY + row * buttonHeight + (row > 0 ? innerPadding : 0);
-                mCurrentGuiModel.centerPanelButtons [i].rect = new Rect(buttonX, buttonY, centerButtonWidth, buttonHeight);
+                currentGUIModel.centerPanelButtons [i].rect = new Rect(buttonX, buttonY, centerButtonWidth, buttonHeight);
                 
             }
 
-            mCurrentGuiModel.cached = true;
+            currentGUIModel.cached = true;
         }
 
         GUIModelManager.Button temp;
 
         //draw left panel icons
-        for (int i = 0, len = mCurrentGuiModel.leftPanelButtons.Count; i < len; i++) {
-            temp = mCurrentGuiModel.leftPanelButtons [i];
+        for (int i = 0, len = currentGUIModel.leftPanelButtons.Count; i < len; i++) {
+            temp = currentGUIModel.leftPanelButtons [i];
             if (temp.text != null) {
                 GUI.Button(temp.rect, temp.text);
             } else {
@@ -247,8 +247,8 @@ public class GUIManager : MonoBehaviour {
             }
         }
         //draw center panel icons
-        for (int i = 0, len = mCurrentGuiModel.centerPanelButtons.Count; i < len; i++) {
-            temp = mCurrentGuiModel.centerPanelButtons [i];
+        for (int i = 0, len = currentGUIModel.centerPanelButtons.Count; i < len; i++) {
+            temp = currentGUIModel.centerPanelButtons [i];
             if (temp.text != null) {
                 GUI.Button(temp.rect, temp.text);
             } else {
@@ -288,25 +288,25 @@ public class GUIManager : MonoBehaviour {
      */
     private void checkIfDragging() {
         if (Input.GetMouseButtonDown(0)) {
-            mDragLocationStart = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            mCheckDeselect = true;
+            dragLocationStart = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            checkSelect = true;
         }
         
         if (Input.GetMouseButtonUp(0)) {
-            mCheckDeselect = false;
-            sDragging = false;
+            checkSelect = false;
+            isDragging = false;
         }
         
-        if (mCheckDeselect) {
-            if (Mathf.Abs(Input.mousePosition.x - mDragLocationStart.x) > 2 && 
-                Mathf.Abs(Input.mousePosition.y - mDragLocationStart.y) > 2) {
-                mCheckDeselect = false;
-                sDragging = true;
+        if (checkSelect) {
+            if (Mathf.Abs(Input.mousePosition.x - dragLocationStart.x) > 2 && 
+                Mathf.Abs(Input.mousePosition.y - dragLocationStart.y) > 2) {
+                checkSelect = false;
+                isDragging = true;
             }
         }
     }
 
-    public void DragBox(Vector2 topLeft, Vector2 bottomRight, GUIStyle style) {
+    public void DragBox(Vector2 topLeft, Vector2 bottomRight, GUISkin dragSkin) {
         float minX = Mathf.Max(topLeft.x, bottomRight.x);
         float maxX = Mathf.Min(topLeft.x, bottomRight.x);
         
@@ -315,7 +315,7 @@ public class GUIManager : MonoBehaviour {
         
         Rect rect = new Rect(minX, minY, maxX - minX, maxY - minY);
 
-        GUI.Box(rect, "", style);
+        GUI.Box(rect, "", dragSkin.box);
     }
 
     /*
@@ -324,16 +324,16 @@ public class GUIManager : MonoBehaviour {
     public static int [] GetButtonID(Vector3 mousePos) {
         int[] button = new int[2];
         Vector2 mousePos2D = new Vector2(mousePos.x, Screen.height - mousePos.y);
-        for (int i = 0; i < mCurrentGuiModel.leftPanelButtons.Count; i++) {
-            if (mCurrentGuiModel.leftPanelButtons [i].rect.Contains(mousePos2D)) {
+        for (int i = 0; i < currentGUIModel.leftPanelButtons.Count; i++) {
+            if (currentGUIModel.leftPanelButtons [i].rect.Contains(mousePos2D)) {
                 button [0] = 0;
                 button [1] = i;
                 return button;
             }
         }
 
-        for (int i = 0; i < mCurrentGuiModel.centerPanelButtons.Count; i++) {
-            if (mCurrentGuiModel.centerPanelButtons [i].rect.Contains(mousePos2D)) {
+        for (int i = 0; i < currentGUIModel.centerPanelButtons.Count; i++) {
+            if (currentGUIModel.centerPanelButtons [i].rect.Contains(mousePos2D)) {
                 button [0] = 1;
                 button [1] = i;
                 return button;
--- KyleSebStuff/RTSGameMechanics/Assets/Scripts/GUI/MoveCommandCursor.cs
+++ KyleSebStuff/RTSGameMechanics/Assets/Scripts/GUI/MoveCommandCursor.cs
@@ -3,17 +3,32 @@ using System.Collections;
 
 public class MoveCommandCursor : MonoBehaviour {
 
-    public Texture2D[] moveCursors;
+    public GUISkin moveCommandSkin;
+    public Texture2D[] moveCommandCursors;
+    public Texture2D activeCursor;
+    private int currentCursorFrame;
     // Use this for initialization
     void Start() {
         Invoke("Destroy", 2f);
-        renderer.material.shader = Shader.Find("Transparent/Diffuse");
     }
-    
-    // Update is called once per frame
-    void Update() {
-        Texture2D currentTexture = moveCursors[(int)(Time.time * 5) % moveCursors.Length];
-        renderer.material.mainTexture = currentTexture;
+
+    void OnGUI() {
+        if(renderer.isVisible)
+            DrawMoveCursor();
+    }
+
+    private void UpdateCursorAnimation() {
+        activeCursor = moveCommandCursors [(int)(Time.time * 10) % moveCommandCursors.Length];
+    }
+
+    private void DrawMoveCursor() {
+        GUI.skin = moveCommandSkin;
+        GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
+        UpdateCursorAnimation();
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        Rect cursorPosition = new Rect(screenPosition.x, screenPosition.y, activeCursor.width, activeCursor.height);
+        GUI.Label(cursorPosition, activeCursor);
+        GUI.EndGroup();
     }
 
     void Destroy() {
