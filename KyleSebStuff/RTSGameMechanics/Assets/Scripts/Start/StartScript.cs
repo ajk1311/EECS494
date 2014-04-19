1b99f8a004681a77629c9ab9b90f43e2a628a8cb
jjhsiung91 <jjhsiung91@gmail.com>
2014-04-18 22:36:24 -0400

    something random

--- KyleSebStuff/RTSGameMechanics/Assets/Scripts/Start/StartScript.cs
+++ KyleSebStuff/RTSGameMechanics/Assets/Scripts/Start/StartScript.cs
@@ -29,7 +29,7 @@ public class StartScript : MonoBehaviour {
 
 	void Start() {
 		Dispatcher.Instance.Register(this);
-		SSGameSetup.ConnectToGame("akausejr", true);
+		SSGameSetup.ConnectToGame("akausejr", false);
 	}
 	
 	[HandlesEvent]
